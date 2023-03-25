92457a61aee33c55d1202c6e26019507a9e5ff07
Sudhan <55418697+SudhanPlayz@users.noreply.github.com>
2023-03-25 15:48:34 +0000

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,38 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+"use client";
+import { useState } from "react"
 
 export default function Home() {
+  const [wallet, setWallet] = useState(null)
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+    <div className="navbar bg-base-100">
+      <div className="flex-none">
+        <span className="text-lg font-bold">Solana NFT Transfer App</span>
+      </div>
+      <div className="flex-1"></div>
+      <div className="flex-none">
+        <button className="btn btn-primary tooltip" data-tooltip="Connect your wallet to continue.">
+          Connect Wallet
+        </button>
+      </div>
+    </div>
+
+    <div className="container grid grid-cols-1 gap-3 p-4 mx-auto">
+      <div className="card bg-neutral shadow-xl">
+        <div className="card-body flex flex-col items-center justify-center">
+          <h2 className="card-title">Transfer NFT</h2>
+          <p className="card-subtitle text-center">{
+              wallet ?
+              `You can transfer your NFT to another wallet address below.` :
+              `Please connect your wallet to continue.`
+          }</p>
+          <div className="flex flex-col items-center justify-center">
+            <img alt="NFT Image" src="https://api.unsplash.com/photos/random" width={250} height={250} />
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
+    </>
   )
 }
