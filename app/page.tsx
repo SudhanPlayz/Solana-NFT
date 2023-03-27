fb8a7788e9b14a5902080e5f5c03dadc2eb8e01b
Sudhan <55418697+SudhanPlayz@users.noreply.github.com>
2023-03-27 15:28:14 +0000

    final changes

--- app/page.tsx
+++ app/page.tsx
@@ -187,7 +187,7 @@ export default function Home() {
                       <div className="text-2xl font-semibold">{nft.data.name}</div>
                       <div className="text-slate-500">Mint: <span className="text-slate-300">{nft.mint.toString().substr(0, 5) + "..." + nft.mint.toString().substr(-5)}</span></div>
                       <div className="flex flex-row text-center">
-                        <div className="m-1 p-1 w-1/2 bg-blue-600 rounded-lg cursor-pointer hover:bg-blue-700" onClick={() => window.open("https://explorer.solana.com/address/" + nft.mint, "_blank")}>View Mint</div>
+                        <div className="m-1 p-1 w-1/2 bg-blue-600 rounded-lg cursor-pointer hover:bg-blue-700" onClick={() => window.open("https://explorer.solana.com/address/" + nft.mint, "_blank")}>View</div>
                         <div className="m-1 p-1 w-1/2 bg-lime-600 rounded-lg cursor-pointer hover:bg-lime-700" onClick={() => transferNFT(nft, publicKey, connection)}>Transfer</div>
                       </div>
                     </div>
